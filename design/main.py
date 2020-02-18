77231385a5f5491f666dbee2fa4d73fa6f847bb8
Mattakyshi <M_Kuklin@users.noreply.github.com>
2020-02-18 15:11:11 +0300

    Added initial design

---
+++ design/main.py
@@ -0,0 +1,93 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.utils import platform
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.properties import ObjectProperty
+import kivy.core.text
+import cv2
+from kivy.base import EventLoop
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.core.window import Window
+import os
+import sys
+
+
+
+class KivyCamera(Image):
+    def __init__(self, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = None
+
+    def start(self, capture, fps=60):
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def stop(self):
+        Clock.unschedule_interval(self.update)
+        self.capture = None
+
+    def update(self, dt):
+        return_value, frame = self.capture.read()
+        if return_value:
+            texture = self.texture
+            w, h = frame.shape[1], frame.shape[0]
+            if not texture or texture.width != w or texture.height != h:
+                self.texture = texture = Texture.create(size=(w, h))
+                texture.flip_vertical()
+            texture.blit_buffer(frame.tobytes(), colorfmt='bgr')
+            self.canvas.ask_update()
+capture = None
+
+class MenuScreen(Screen):
+    def init_qrtest(self):
+        pass
+
+    def dostart(self, *largs):
+        global capture
+        capture = cv2.VideoCapture(0)
+        self.ids.qrcam.start(capture)
+
+    def doexit(self):
+        global capture
+        if capture != None:
+            capture.release()
+            capture = None
+            python = sys.executable
+            os.execl(python, python, * sys.argv)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._request_android_permissions()
+
+    @staticmethod
+    def is_android():
+        return platform == 'android'
+
+    def _request_android_permissions(self):
+        """
+        Requests CAMERA permission on Android.
+        """
+        if not self.is_android():
+            return
+        from android.permissions import request_permission, Permission
+        request_permission(Permission.CAMERA)
+
+
+class SettingsScreen(Screen):
+    pass
+
+class ScreenManager(ScreenManager):
+    pass
+
+#designedbymattakyshi
+buildKV = Builder.load_file("Test.kv")
+class TestApp(App):
+    def build(self):
+        return buildKV
+
+if __name__ == '__main__':
+    TestApp().run()
